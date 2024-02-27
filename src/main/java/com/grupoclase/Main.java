67017c7e16ae4b5e6a7f1a10a0bf15e3fbbf341c
Jon Ander <jonander2233@gmail.com>
2024-02-27 23:02:38 +0100

    creada mi rama

--- src/main/java/com/grupoclase/Main.java
+++ src/main/java/com/grupoclase/Main.java
@@ -2,6 +2,7 @@ package com.grupoclase;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Rama de Jon
 public class Main {
     public static void main(String[] args) {
 
