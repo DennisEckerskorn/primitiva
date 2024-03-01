711a99005aeabf3cfb829cf87590ad063ea8be9c
Jon Ander <jonander2233@gmail.com>
2024-03-01 14:37:04 +0100

    empezado clase bombo

--- src/main/java/com/grupoclase/resources/Bombo.java
+++ src/main/java/com/grupoclase/resources/Bombo.java
@@ -1,5 +1,32 @@
 package com.grupoclase.resources;
-//Jon
-public class Bombo {
 
+import com.grupoclase.dynamicarray.GenericDynamicArray;
+
+import java.util.Random;
+
+//jon
+public class Bombo {
+    int CANTIDAD_NUMS_BOMBO_GRANDE = 49-1;
+    int CANTIDAD_NUMS_BOMBO_PEQUENO = 10-1;
+
+    GenericDynamicArray bomboGrande;
+    GenericDynamicArray bomboPequeno;
+
+    public Bombo() {
+        bomboGrande = new GenericDynamicArray(CANTIDAD_NUMS_BOMBO_GRANDE);
+        bomboPequeno = new GenericDynamicArray(CANTIDAD_NUMS_BOMBO_PEQUENO);
+        Random random = new Random();
+        for (int i = 0; i < CANTIDAD_NUMS_BOMBO_GRANDE; i++) {
+            int num = i;
+            bomboGrande.add(num+1);
+
+        }
+        for (int i = 0; i < CANTIDAD_NUMS_BOMBO_PEQUENO; i++) {
+            int num = i;
+            bomboPequeno.add(num+1);
+        }
+    }
+    private int extraerNum(){
+           return 1;
+    }
 }
