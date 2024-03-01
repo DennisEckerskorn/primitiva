8f9b5fec9b533982834a147c83016f31c6a7172c
Jon <jonander2233@gmail.com>
2024-03-01 16:38:59 +0100

    wip bombo

--- src/main/java/com/grupoclase/resources/Bombo.java
+++ src/main/java/com/grupoclase/resources/Bombo.java
@@ -6,16 +6,16 @@ import java.util.Random;
 
 //jon
 public class Bombo {
-    int CANTIDAD_NUMS_BOMBO_GRANDE = 49-1;
-    int CANTIDAD_NUMS_BOMBO_PEQUENO = 10-1;
+    private int CANTIDAD_NUMS_BOMBO_GRANDE = 50;
+    private int CANTIDAD_NUMS_BOMBO_PEQUENO = 10;
     private Random random = new Random();
 
-    GenericDynamicArray bomboGrande;
-    GenericDynamicArray bomboPequeno;
+    private GenericDynamicArray bomboGrande;
+    private GenericDynamicArray bomboPequeno;
 
     public Bombo() {
-        bomboGrande = new GenericDynamicArray(CANTIDAD_NUMS_BOMBO_GRANDE);
-        bomboPequeno = new GenericDynamicArray(CANTIDAD_NUMS_BOMBO_PEQUENO);
+        bomboGrande = new GenericDynamicArray();
+        bomboPequeno = new GenericDynamicArray();
         for (int i = 0; i < CANTIDAD_NUMS_BOMBO_GRANDE; i++) {
             int num = i;
             bomboGrande.add(num+1);
@@ -29,10 +29,35 @@ public class Bombo {
     private int extraerNum(){
            return 1;
     }
-    public int[] extraerCombinacionGanadora (){
-        //TODO metodo que haga un random al array de bombo grande sin repetir la posición del array
 
-        int[] arrayPrueba = new int[]{1,2,3};
-        return arrayPrueba;
+    public void resetBombo(){
+        GenericDynamicArray array1 = new GenericDynamicArray();
+        GenericDynamicArray array2 = new GenericDynamicArray();
+
+        for (int i = 0; i < CANTIDAD_NUMS_BOMBO_GRANDE; i++) {
+            int num = i;
+            array1.add(num+1);
+
+        }
+        for (int i = 0; i < CANTIDAD_NUMS_BOMBO_PEQUENO; i++) {
+            int num = i;
+            array2.add(num+1);
+
+        }
+        bomboGrande = array1;
+        bomboPequeno = array2;
+    }
+
+
+    public int[] extraerCombinacionGanadora (int cantidadNums){
+
+        int[] arrayGanadores = new int[cantidadNums];
+
+        for (int i = 0; i < cantidadNums; i++) {
+            int posElegida = random.nextInt(bomboGrande.size());
+            int numElegido = bomboGrande.remove(posElegida);
+            arrayGanadores[i] = numElegido;
+        }
+        return arrayGanadores;
     }
 }
