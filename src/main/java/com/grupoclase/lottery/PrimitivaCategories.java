b0e0933371606b950ee0b32b86289016dab619e4
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2024-03-11 23:19:42 +0100

    subida de clase primitivacategories

---
+++ src/main/java/com/grupoclase/lottery/PrimitivaCategories.java
@@ -0,0 +1,13 @@
+package com.grupoclase.lottery;
+
+
+    public enum PrimitivaCategories {
+        JUEGO_UNICO, // Juego único
+        JUGAR_HASTA_PREMIO, // Jugar hasta obtener premio
+        JUGAR_HASTA_PREMIO_SIN_REINTEGRO, // Jugar hasta obtener premio (sin reintegro)
+        CICLO_10000_SORTEOS, // Ciclo de 10000 sorteos
+        JUGAR_HASTA_PREMIO_ESPECIAL // Jugar hasta obtener premio de categoría especial
+        ;
+
+    }
+
